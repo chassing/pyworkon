5e0b031f9bd546a0da3301de4ecc3d17c2d6f4bc
Christian Assing <christian.assing@infineon.com>
2022-04-05 08:43:29 +0200

    py3.9 fix

--- pyworkon/providers/__init__.py
+++ pyworkon/providers/__init__.py
@@ -1,10 +1,11 @@
+from typing import Union
 from ..config import Provider, ProviderType
 from ..exceptions import UnknownProviderType
 from .github.github import GitHubApi
 from .gitlab.gitlab import GitLabApi
 
 
-def get_provider(provider: Provider) -> GitHubApi | GitLabApi:
+def get_provider(provider: Provider) -> Union[GitHubApi, GitLabApi]:
     if provider.type == ProviderType.github:
         return GitHubApi(url=provider.api_url, username=provider.username, password=provider.password)
     raise UnknownProviderType(f"{provider.name}: {provider.type=} not supported")
