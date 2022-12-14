d5e4f76a88f4693df02ed1ce4b2181ab2de2b643
Christian Assing <cassing@redhat.com>
2022-12-14 13:18:27 +0100

    fix(gitlab): default_branch is optional

--- pyworkon/providers/gitlab/models.py
+++ pyworkon/providers/gitlab/models.py
@@ -16,13 +16,15 @@ class Repository(BaseModel):
     path: str  # 'pyworkon'
     path_with_namespace: str  # 'assing/pyworkon'
     created_at: datetime
-    default_branch: str  # 'main'
+    default_branch: Optional[str] = "main"
     # tag_list: []
     # topics: []
     ssh_url_to_repo: str  # 'git@gitlab.com:assing/pyworkon.git'
     http_url_to_repo: HttpUrl  # 'https://gitlab.com/assing/pyworkon.git'
     web_url: HttpUrl  # 'https://gitlab.com/assing/pyworkon'
-    readme_url: HttpUrl | None = None  # 'https://gitlab.com/assing/pyworkon/-/blob/main/README.md'
+    readme_url: HttpUrl | None = (
+        None  # 'https://gitlab.com/assing/pyworkon/-/blob/main/README.md'
+    )
     avatar_url: Optional[HttpUrl] = None
     forks_count: int  # 0
     star_count: int  # 1
